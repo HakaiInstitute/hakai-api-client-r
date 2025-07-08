b233ff099625f3ad527cef64c40bf640a32cd689
Sam Albers <sam.albers@hakai.org>
2025-07-08 10:37:39 -0700

    run air

--- hakaiApi/R/client.R
+++ hakaiApi/R/client.R
@@ -3,7 +3,7 @@
 #' @description
 #' Class to use to make authenticated API requests for Hakai data
 #' @importFrom R6 R6Class
-#' @importFrom httr2 request req_headers req_method req_body_json req_perform 
+#' @importFrom httr2 request req_headers req_method req_body_json req_perform
 #' @importFrom readr type_convert
 #' @importFrom tibble as_tibble
 #' @importFrom dplyr bind_rows
@@ -14,7 +14,7 @@
 #' try(
 #'   client <- Client$new()
 #' )
-#' 
+#'
 #' # Follow authorization prompts to log in
 #'
 #' # Retrieve some data. See <https://hakaiinstitute.github.io/hakai-api/> for options.
@@ -38,7 +38,8 @@
 #' #  4 Cruickshank WS
 #' #  ...
 #' }
-Client <- R6::R6Class("Client",  # nolint
+Client <- R6::R6Class(
+  "Client", # nolint
   lock_objects = FALSE,
   public = list(
     #' @field api_root The api_root you are logged in to
@@ -54,8 +55,10 @@ Client <- R6::R6Class("Client",  # nolint
     #' try(
     #'    client <- Client$new()
     #' )
-    initialize = function(api_root = "https://hecate.hakai.org/api",
-                          login_page="https://hecate.hakai.org/api-client-login") {
+    initialize = function(
+      api_root = "https://hecate.hakai.org/api",
+      login_page = "https://hecate.hakai.org/api-client-login"
+    ) {
       self$api_root <- api_root
       private$login_page_url <- login_page
       private$credentials_file <- path.expand("~/.hakai-api-auth-r")
@@ -72,14 +75,16 @@ Client <- R6::R6Class("Client",  # nolint
     #'@description
     #' Send a GET request to the API
     #' @param endpoint_url The full API url to fetch data from
-    #' @param col_types a readr type specification  
+    #' @param col_types a readr type specification
     #' @return A dataframe of the requested data
     #' @examples
     #' try(client$get("https://hecate.hakai.org/api/aco/views/projects"))
     get = function(endpoint_url, col_types = NULL) {
-      token <- paste(private$credentials$token_type,
-                     private$credentials$access_token)
-      r <- base_request(endpoint_url, token) |> 
+      token <- paste(
+        private$credentials$token_type,
+        private$credentials$access_token
+      )
+      r <- base_request(endpoint_url, token) |>
         httr2::req_perform()
       data <- httr2::resp_body_json(r)
       data <- private$json2tbl(httr2::resp_body_json(r))
@@ -94,13 +99,19 @@ Client <- R6::R6Class("Client",  # nolint
     #' @param rec_data dataframe, list, or other R data structure to send as part of the post request payload
     #' @return post request response status code and description
     post = function(endpoint_url, rec_data) {
-      token <- paste(private$credentials$token_type,
-                     private$credentials$access_token)
+      token <- paste(
+        private$credentials$token_type,
+        private$credentials$access_token
+      )
       resp <- base_request(endpoint_url, token) |>
         httr2::req_method("POST") |>
         httr2::req_body_json(rec_data) |>
         httr2::req_perform()
-      data <- paste0(httr2::resp_status(resp), ' ',  httr2::resp_status_desc(resp))
+      data <- paste0(
+        httr2::resp_status(resp),
+        ' ',
+        httr2::resp_status_desc(resp)
+      )
       return(data)
     },
 
@@ -110,13 +121,19 @@ Client <- R6::R6Class("Client",  # nolint
     #' @param rec_data dataframe, list, or other R data structure to send as part of the post request payload
     #' @return PUT request response status code and description
     put = function(endpoint_url, rec_data) {
-      token <- paste(private$credentials$token_type,
-                     private$credentials$access_token)
+      token <- paste(
+        private$credentials$token_type,
+        private$credentials$access_token
+      )
       resp <- base_request(endpoint_url, token) |>
         httr2::req_body_json(data = rec_data, auto_unbox = TRUE) |>
         httr2::req_method("PUT") |>
         httr2::req_perform()
-      data <- paste0(httr2::resp_status(resp), ' ',  httr2::resp_status_desc(resp))
+      data <- paste0(
+        httr2::resp_status(resp),
+        ' ',
+        httr2::resp_status_desc(resp)
+      )
       return(resp)
     },
 
@@ -126,13 +143,19 @@ Client <- R6::R6Class("Client",  # nolint
     #' @param rec_data dataframe, list, or other R data structure to send as part of the post request payload
     #' @return PATCH request response status code and description
     patch = function(endpoint_url, rec_data) {
-      token <- paste(private$credentials$token_type,
-                     private$credentials$access_token)
+      token <- paste(
+        private$credentials$token_type,
+        private$credentials$access_token
+      )
       resp <- base_request(endpoint_url, token) |>
         httr2::req_body_json(data = rec_data, auto_unbox = TRUE) |>
         httr2::req_method("PATCH") |>
         httr2::req_perform()
-      data <- paste0(httr2::resp_status(resp), ' ',  httr2::resp_status_desc(resp))
+      data <- paste0(
+        httr2::resp_status(resp),
+        ' ',
+        httr2::resp_status_desc(resp)
+      )
       return(resp)
     },
 
@@ -156,19 +179,26 @@ Client <- R6::R6Class("Client",  # nolint
       json2tbl_impl(data)
     },
     querystring2df = function(querystring) {
-      tryCatch({
-        s <- strsplit(querystring, "&")
-        p <- lapply(s, function(a) {
-          strsplit(a, "=")
-        })
-        df <- data.frame(matrix(unlist(p), ncol = length(unlist(s))))
-        names(df) <- as.character(unlist(df[1, ]))
-        return(df[-1, ])
-      }, error = function(e) {
-        # Return dummy credentials
-        writeLines("Invalid credential format, try again.")
-        return(data.frame(token_type = "", access_token = "", expires_at = -1))
-      })
+      tryCatch(
+        {
+          s <- strsplit(querystring, "&")
+          p <- lapply(s, function(a) {
+            strsplit(a, "=")
+          })
+          df <- data.frame(matrix(unlist(p), ncol = length(unlist(s))))
+          names(df) <- as.character(unlist(df[1, ]))
+          return(df[-1, ])
+        },
+        error = function(e) {
+          # Return dummy credentials
+          writeLines("Invalid credential format, try again.")
+          return(data.frame(
+            token_type = "",
+            access_token = "",
+            expires_at = -1
+          ))
+        }
+      )
     },
     get_credentials_from_web = function() {
       # Get the user to login and get the oAuth2 code from the redirect url
@@ -176,7 +206,9 @@ Client <- R6::R6Class("Client",  # nolint
       writeLines(private$login_page_url)
       writeLines("")
 
-      querystring <- readline("Copy and past your credentials from the login page:\n")
+      querystring <- readline(
+        "Copy and past your credentials from the login page:\n"
+      )
       credentials <- private$querystring2df(querystring)
       return(credentials)
     },
@@ -186,24 +218,26 @@ Client <- R6::R6Class("Client",  # nolint
         return(FALSE)
       }
 
-      tryCatch({
-        # Load the credentials from the file cache
-        credentials_file <- file(private$credentials_file, "r")
-        credentials <- unserialize(credentials_file)
-        close(credentials_file)
+      tryCatch(
+        {
+          # Load the credentials from the file cache
+          credentials_file <- file(private$credentials_file, "r")
+          credentials <- unserialize(credentials_file)
+          close(credentials_file)
 
-        # Check that credentials aren't expired
-        if (as.numeric(Sys.time()) > credentials$expires_at) {
+          # Check that credentials aren't expired
+          if (as.numeric(Sys.time()) > credentials$expires_at) {
+            self$remove_credentials()
+            credentials <- FALSE
+          }
+        },
+        error = function(cond) {
+          message("Error reading cached credentials:")
+          message(cond[1])
           self$remove_credentials()
-          credentials <- FALSE
+          credentials <<- FALSE
         }
-      },
-      error = function(cond) {
-        message("Error reading cached credentials:")
-        message(cond[1])
-        self$remove_credentials()
-        credentials <<- FALSE
-      })
+      )
 
       # If all is well, return the credentials
       return(credentials)
--- hakaiApi/R/utils.R
+++ hakaiApi/R/utils.R
@@ -1,9 +1,9 @@
 base_request <- function(endpoint_url, token) {
   user_agent <- Sys.getenv("HAKAI_API_USER_AGENT", "hakai-api-client-r")
-  
+
   httr2::request(endpoint_url) |>
     httr2::req_headers("Authorization" = token) |>
-    httr2::req_user_agent(user_agent) 
+    httr2::req_user_agent(user_agent)
 }
 
 
@@ -13,9 +13,9 @@ json2tbl_impl <- function(data) {
     return(tibble::tibble(value = unlist(data)))
   }
   data <- lapply(data, function(data) {
-    data[sapply(data, is.null)] <- NA  # nolint
+    data[sapply(data, is.null)] <- NA # nolint
     unlist(data)
   })
-  data <- bind_rows(data)
+  data <- dplyr::bind_rows(data)
   return(data)
 }
